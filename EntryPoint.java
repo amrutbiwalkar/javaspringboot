d98f3296a228184e28ebcea64f997bfc05d8791f
amrutbiwalkar <amrut.biwalkar@gmail.com>
2023-04-19 18:06:26 +0530

    First Java

---
+++ EntryPoint.java
@@ -0,0 +1,13 @@
+
+public class EntryPoint {
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		
+		Maths m = new Maths();
+				m.sum(12, 24);
+				m.sub(30, 20);
+				m.multi(4, 5);
+				m.div(24, 2);
+	}
+
+}
---
+++ Maths.java
@@ -0,0 +1,27 @@
+
+public class Maths {
+
+	public void sum (int a,int b)
+	{
+		int c =a+b;
+		System.out.println("Sum of  your number:" + c);
+	}
+	
+	public void sub (int a,int b)
+	{
+		int c =a-b;
+		System.out.println("Subtraction of  your number:" + c);
+	}
+	
+	public void multi (int a,int b)
+	{
+		int c =a*b;
+		System.out.println("Multiplication of  your number:" + c);
+	}
+	
+	public void div (int a,int b)
+	{
+		int c =a/b;
+		System.out.println("Division of the your number:" + c);
+	}
+}
